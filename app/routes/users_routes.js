546f8a0cd11b3d5c78b3f6a23bfa0e5b6a9ebdda
maestro-game <super.belovvadim@yandex.ru>
2020-07-20 02:19:19 +0300

    8. Реализовать функционал по отрправке JSON-формы

--- app/routes/users_routes.js
+++ app/routes/users_routes.js
@@ -1,6 +1,8 @@
+bodyParser = require('body-parser').json();
+
 module.exports = function (app) {
     app.get('/profile', (req, res) => {
-        var result = [
+        let result = [
             {
                 "s1p": "Я усердный, усидчивый, устремленный, уверенный, упорный",
                 "s1": "Обо мне",
@@ -13,5 +15,40 @@ module.exports = function (app) {
             }
         ];
         res.send(JSON.stringify(result));
+    })
+
+    app.post('/send', (req, res) => {
+        const {Pool} = require('pg')
+        const fs = require('fs')
+        let body = req.body;
+        const pool = new Pool({
+            user: 'postgres',
+            host: 'localhost',
+            database: 'nodejs',
+            password: 'sdfsdf',
+            port: 5432,
+        })
+        pool.query("insert into letters values (default, '" + body['email'] + "', '" + body['name'] + "', '" + body['message'] + "')", (err) => {
+            pool.end()
+        })
+        const java = fs.readFileSync('static/submit.html')
+        res.setHeader("Content-Type", "text/html")
+        res.send(java)
+    })
+
+    app.get('/letters', (request, result) => {
+        const {Pool} = require('pg')
+        let body = request.body;
+        const pool = new Pool({
+            user: 'postgres',
+            host: 'localhost',
+            database: 'nodejs',
+            password: 'sdfsdf',
+            port: 5432,
+        })
+        pool.query("select * from letters", (err, res) => {
+            result.send(JSON.stringify(res["rows"]));
+            pool.end()
+        })
     })
 };
